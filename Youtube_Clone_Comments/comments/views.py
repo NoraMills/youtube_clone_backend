37b3b144dcd0aa9e46b297e75b8e95e028023268
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-18 14:03:41 -0500

    serializers set up, views started

--- Youtube_Clone_Comments/comments/views.py
+++ Youtube_Clone_Comments/comments/views.py
@@ -1,3 +1,18 @@
+from Youtube_Clone_Comments.comments.serializers import CommentSerializer
+from Youtube_Clone_Comments.comments.models import Comment
 from django.shortcuts import render
+from rest_framework.views import APIView
+# serializers
+from rest_framework.response import Response
+from rest_framework import serializers, status
+# from django.http import Http404 ?
 
 # Create your views here.
+
+
+class Comments(APIView):
+
+    def get(self, request):
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data)
