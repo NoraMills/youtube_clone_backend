6cf901e9015f1c85bbad21ef83a85a8b9c7733c3
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-19 09:59:51 -0500

    corrected spelling issue with dislikes that was preventing GET working

--- Youtube_Clone_Comments/comments/models.py
+++ Youtube_Clone_Comments/comments/models.py
@@ -8,8 +8,14 @@ class Comment(models.Model):
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.video_id
+
 
 class Reply(models.Model):
     comment = models.ForeignKey(
         Comment, blank=True, null=True, on_delete=models.CASCADE)
     body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.comment
--- Youtube_Clone_Comments/comments/serializers.py
+++ Youtube_Clone_Comments/comments/serializers.py
@@ -5,7 +5,7 @@ from .models import Comment, Reply
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'video_id', 'body', 'likes', 'dislike']
+        fields = ['id', 'video_id', 'body', 'likes', 'dislikes']
 
 
 class ReplySerializer(serializers.ModelSerializer):
